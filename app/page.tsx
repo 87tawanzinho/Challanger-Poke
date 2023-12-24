690625dede958bc40ef89e63bbbe3f3f346a8579
tawan <87tawanzinho@gmail.com>
2023-12-24 18:18:03 -0300

    progress

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,36 @@
+"use client";
+import axios from "axios";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 //Consumir a API e listar todos os pokémons que a consulta do seguinte endpoint retornar: https://pokeapi.co/api/v2/pokemon
 
 //Você deve exibir NOME, EXPERIÊNCIA e IMAGEM de cada pokémon.
 export default function Home() {
-  return <main></main>;
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon")
+      .then((response) => setData(response.data.results));
+  }, []);
+  return (
+    <main>
+      {data.map((item) => (
+        <Pokemon data={item} />
+      ))}
+    </main>
+  );
 }
+
+const Pokemon = ({ data }) => {
+  const [details, setDetails] = useState(null);
+
+  useEffect(() => {
+    axios.get(data.url).then((response) => setDetails(response.data));
+  }, []);
+  if (details === null) {
+    return <div></div>;
+  }
+
+  return <div>{JSON.stringify(details.name)}</div>;
+};
