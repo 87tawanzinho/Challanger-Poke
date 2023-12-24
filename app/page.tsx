b8284e7c13ad6c73167452c75b22f56d3dfdeccb
tawan <87tawanzinho@gmail.com>
2023-12-24 18:35:32 -0300

    already finished

--- app/page.tsx
+++ app/page.tsx
@@ -2,28 +2,49 @@
 import axios from "axios";
 import Image from "next/image";
 import { useEffect, useState } from "react";
+interface PokemonDetails {
+  name: string;
+  base_experience: number; // Assuming base_experience is a number
+  sprites: {
+    front_default: string;
+  };
+  url: string;
+}
+interface Dating {
+  name: string;
+  url: string;
+}
 
 //Consumir a API e listar todos os pokémons que a consulta do seguinte endpoint retornar: https://pokeapi.co/api/v2/pokemon
 
 //Você deve exibir NOME, EXPERIÊNCIA e IMAGEM de cada pokémon.
 export default function Home() {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState<Dating[]>([]);
+
+  const fetchData = () => {
+    axios.get("https://pokeapi.co/api/v2/pokemon").then((response) => {
+      const arrayFiltered = [...response.data.results];
+      console.log(response.data.results);
+      arrayFiltered.sort((a, b) => {
+        return a.name.localeCompare(b.name);
+      });
+      setData(arrayFiltered);
+    });
+  };
   useEffect(() => {
-    axios
-      .get("https://pokeapi.co/api/v2/pokemon")
-      .then((response) => setData(response.data.results));
+    fetchData();
   }, []);
   return (
     <main>
       {data.map((item) => (
-        <Pokemon data={item} />
+        <Pokemon key={item.name} data={item} />
       ))}
     </main>
   );
 }
 
-const Pokemon = ({ data }) => {
-  const [details, setDetails] = useState(null);
+const Pokemon = ({ data }: { data: PokemonDetails }) => {
+  const [details, setDetails] = useState<PokemonDetails | null>(null);
 
   useEffect(() => {
     axios.get(data.url).then((response) => setDetails(response.data));
@@ -32,5 +53,15 @@ const Pokemon = ({ data }) => {
     return <div></div>;
   }
 
-  return <div>{JSON.stringify(details.name)}</div>;
+  return (
+    <div>
+      <span>
+        {" "}
+        <b>{JSON.stringify(details.name)}</b>- EXP {details.base_experience}
+      </span>
+      <span>
+        <img src={details.sprites.front_default} alt="" />
+      </span>
+    </div>
+  );
 };
